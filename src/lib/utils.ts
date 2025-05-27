ebedde3f1a151a340358709c0dbc4dbfdbe30aad
Sofia <unitargetofsofi@gmail.com>
2025-05-27 18:34:25 +0200

    format utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,6 @@
-
 import { TransactionStatus } from "@/contexts/DataContext";
+import { Transaction } from "@/types/transactions";
+import { AxiosResponse } from "axios";
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
@@ -7,12 +8,15 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export function formatCurrency(amount: number, useSymbol: boolean = true): string {
-  return new Intl.NumberFormat('es-CO', {
-    style: 'currency',
-    currency: 'COP',
+export function formatCurrency(
+  amount: number,
+  useSymbol: boolean = true
+): string {
+  return new Intl.NumberFormat("es-CO", {
+    style: "currency",
+    currency: "COP",
     maximumFractionDigits: 0,
-    currencyDisplay: useSymbol ? 'symbol' : 'code',
+    currencyDisplay: useSymbol ? "symbol" : "code",
   }).format(amount);
 }
 
@@ -31,7 +35,41 @@ export const mapStatusToSpanish = (status: string): TransactionStatus => {
     case "canceled": // Mapea "Canceled" a "rejected"
       return "Rechazado";
     default:
-      console.warn(`Estado de transacción desconocido: ${status}. Mapeando a pending.`);
+      console.warn(
+        `Estado de transacción desconocido: ${status}. Mapeando a pending.`
+      );
       return "Pendiente";
   }
+};
+
+export const parseTransactionsResponse = (response: AxiosResponse): Transaction[]  => {
+  let transactions: Transaction[] = [];
+
+  if (Array.isArray(response.data)) {
+    transactions = response.data;
+  } else if (response.data && typeof response.data === "object") {
+    // If it's an object with a data property that is an array
+    if (Array.isArray(response.data.data)) {
+      transactions = response.data.data;
+    } else {
+      // If it's a single transaction object, wrap it in an array
+      transactions = [response.data].filter(
+        (item) => item && typeof item === "object"
+      );
+    }
+  }
+  if (Array.isArray(response.data)) {
+    transactions = response.data;
+  } else if (response.data && typeof response.data === "object") {
+    // If it's an object with a data property that is an array
+    if (Array.isArray(response.data.data)) {
+      transactions = response.data.data;
+    } else {
+      // If it's a single transaction object, wrap it in an array
+      transactions = [response.data].filter(
+        (item) => item && typeof item === "object"
+      );
+    }
+  }
+  return transactions
 };
